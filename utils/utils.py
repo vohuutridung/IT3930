28f6ef38e264dd7385124ee252f038fdf63167b7
Vo Huu Tri Dung <tridung02092005@gmail.com>
2026-05-22 19:23:15 +0700

    feat: implement TaskVector class for model merging and add parameter filtering utility

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import torch
+import re
 
 def set_random_seed(seed: int = 0):
     random.seed(seed)
@@ -10,3 +11,17 @@ def set_random_seed(seed: int = 0):
         torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+def get_param_names_to_merge(input_param_names: list, exclude_param_names_regex: list):
+    """
+    get the names of parameters that need to be merged
+    :param input_param_names: list, names of input parameters
+    :param exclude_param_names_regex: list, regular expression of names of parameters that need to be excluded
+    :return:
+    """
+    param_names_to_merge = []
+    for param_name in input_param_names:
+        exclude = any([re.match(exclude_pattern, param_name) for exclude_pattern in exclude_param_names_regex])
+        if not exclude:
+            param_names_to_merge.append(param_name)
+    return param_names_to_merge
